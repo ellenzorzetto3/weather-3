f92d13de93295ddb9483b1defb22ea18dac852aa
Ellen Zorzetto <ellen.zorzetto.it@gmail.com>
2024-05-08 10:24:02 -0300

    changed the API key

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
